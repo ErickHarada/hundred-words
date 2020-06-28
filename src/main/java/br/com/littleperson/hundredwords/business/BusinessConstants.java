4b5753f640751f938cef8b16e5574d9bdba158a4
Erick <eefeaga2000@gmail.com>
2020-06-28 16:00:35 -0300

    Diminuindo o numero de palavras a serem consultadas

--- src/main/java/br/com/littleperson/hundredwords/business/BusinessConstants.java
+++ src/main/java/br/com/littleperson/hundredwords/business/BusinessConstants.java
@@ -2,9 +2,9 @@ package br.com.littleperson.hundredwords.business;
 
 public class BusinessConstants {
 
-    public static final Integer MAX_VALUE = 20;
+    public static final Integer MAX_VALUE = 25;
 
-    public static final Integer QUANTITY_WORDS = 8;
+    public static final Integer QUANTITY_WORDS = 2;
 
     public static final String TIME_ZONE = "America/Sao_Paulo";
 }
